3c929e73d21c149038e32b74bfe58b316950d985
nil55 <whonils@gmail.com>
2014-01-27 14:02:47 +0100

    made some piratee changes

--- src/nils_krille_java/Nils_krille_java.java
+++ src/nils_krille_java/Nils_krille_java.java
@@ -29,7 +29,7 @@ public class Nils_krille_java {
             System.out.print("\n");
             if (x <= 0) {
                 System.out.print("fuck this shit\n");
-                System.out.print("fuck this shit\n");
+                System.out.print("ye ye some rum me like ajaj\n");
                 System.exit(0);
             }
         }
