8fe5ca449c5c84ffcece87fe894d123b5a5153af
nil55 <whonils@gmail.com>
2014-01-27 13:49:03 +0100

    i did some starting code

---
+++ src/nils_krille_java/Nils_krille_java.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package nils_krille_java;
+
+/**
+ *
+ * @author nils
+ */
+public class Nils_krille_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double x = 100000;
+        while (true) {
+            System.out.print(x);
+            x--;
+            if (x % 2 == 0) {
+                System.out.print(" <--mod 2");
+            }
+            if (x % 3 == 0) {
+                System.out.print(" <--mod 3");
+            }
+            System.out.print("\n");
+            if (x <= 0) {
+                System.out.print("fuck this shit\n");
+                System.exit(0);
+            }
+        }
+    }
+
+}
